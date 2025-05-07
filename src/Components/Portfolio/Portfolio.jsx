8df6e6b82434ebd106668b4a799dcb374fc595e4
adelshokry15 <adelshokry15@yahoo.com>
2025-05-07 19:33:34 +0300

    new

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faL, faStar, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCamera,
+  faL,
+  faStar,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import dashboardImg from "../../assets/images/dash.png";
 import freshcartImg from "../../assets/images/freshcart.png";
 import gamesImg from "../../assets/images/game.png";
 import foodImg from "../../assets/images/yummy.png";
 import stImg from "../../assets/images/st.png";
 import moviesImg from "../../assets/images/mo.png";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "./Portfolio.css";
-import { Link } from "react-router-dom";
 
 const imagesList = [
   {
@@ -40,7 +43,7 @@ const imagesList = [
     id: 5,
     src: stImg,
     alt: "Streetsuite Website",
-    link: "www.streetsuite.com",
+    link: "https://www.streetsuite.com",
   },
   {
     id: 6,
@@ -52,31 +55,7 @@ const imagesList = [
 
 export default function Portfolio() {
   const [link, setLink] = useState(null);
-  function handleImg(e) {
-    if (
-      e.target.classList.contains("layer") ||
-      e.target.classList.contains("flag")
-    ) {
-      console.log(e.target);
-      document
-        .querySelector(".img-display")
-        .classList.replace("d-none", "d-flex");
-      console.log(e.target.dataset.src);
-      document
-        .querySelector(".img-display img")
-        .setAttribute("src", e.target.dataset.src);
-    }
-  }
-  function closeImg(e) {
-    if (
-      e.target.classList.contains("img-display") ||
-      e.target.classList.contains("closemark")
-    ) {
-      document
-        .querySelector(".img-display")
-        .classList.replace("d-flex", "d-none");
-    }
-  }
+  const [image, setImage] = useState(null);
 
   return (
     <>
@@ -95,7 +74,7 @@ export default function Portfolio() {
                   <img className="w-100 rounded-3" src={i.src} alt={i.alt} />
                   <div
                     onClick={(e) => {
-                      handleImg(e);
+                      setImage(i.src);
                       setLink(i.link);
                     }}
                     className="layer"
@@ -103,8 +82,12 @@ export default function Portfolio() {
                   >
                     <FontAwesomeIcon
                       data-src={i.src}
+                      onClick={(e) => {
+                        setImage(i.src);
+                        setLink(i.link);
+                      }}
                       className="flag display-2 fw-bolder"
-                      icon={faPlus}
+                      icon={faCamera}
                       style={{ color: "#ffffff" }}
                     />
                   </div>
@@ -115,32 +98,29 @@ export default function Portfolio() {
         </div>
       </div>
       <div
-        onClick={(e) => {
-          closeImg(e);
-        }}
-        className="img-display position-fixed justify-content-center align-items-center d-none flex-column"
+        className={`img-display position-fixed justify-content-center align-items-center ${
+          image ? "d-flex" : "d-none"
+        } flex-column`}
       >
         <FontAwesomeIcon
           onClick={() => {
             setLink(null);
-            document
-              .querySelector(".img-display")
-              .classList.replace("d-flex", "d-none");
+            setImage(null);
           }}
           className=" display-2 fw-bolder position-absolute closemark"
           icon={faXmark}
           style={{ color: "#ffffff" }}
         />
-        <div style={{ width: "60%" }}>
-          <Link
-          target="_blank"
-            to={link || ""}
+        <div style={{ width: "70%" }}>
+          <a
+            href={link || "#"}
+            target="_blank"
             className="linkwebsite fw-bold display-6 text-white text-decoration-underline mb-1 d-block"
           >
             Click To Go To Website
-          </Link>
+          </a>
         </div>
-        <img src={""} alt="" className="object-fit-cover" />
+        <img src={image || ""} alt="" className="object-fit-cover" />
       </div>
     </>
   );
